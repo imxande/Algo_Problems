7460aade12d2fa57d7fa4621e791349eefe170a5
imxande <alerodriguezlarosa@gmail.com>
2020-07-13 21:34:32 -0400

    two sum O(nlogn) time implemented

--- array_problems/twoNumberSum.js
+++ array_problems/twoNumberSum.js
@@ -8,38 +8,38 @@
     Ex: input => const targetSum = 10
 */
 
-// // O(n^2) time complexity, O(n) space complecity
-// const twoSum = (array, target) => {
-//   // placeholder for our result
-//   const result = [];
+// O(n^2) time complexity, O(n) space complecity
+const twoSumOne = (array, target) => {
+  // placeholder for our result
+  const result = [];
 
-//   //  outer loop to get first number
-//   for (let i = 0; i < array.length; i++) {
-//     //   grab first number
-//     const firstNumber = array[i];
+  //  outer loop to get first number
+  for (let i = 0; i < array.length; i++) {
+    //   grab first number
+    const firstNumber = array[i];
 
-//     // inner loop to grab second number and get the current sum
-//     for (let j = i + 1; j < array.length; j++) {
-//       // grab second number
-//       const secondNumber = array[j];
+    // inner loop to grab second number and get the current sum
+    for (let j = i + 1; j < array.length; j++) {
+      // grab second number
+      const secondNumber = array[j];
 
-//       //   get current sum
-//       const currentSum = firstNumber + secondNumber;
+      //   get current sum
+      const currentSum = firstNumber + secondNumber;
 
-//       //   check if the current sum is equal to the target
-//       if (currentSum === target) {
-//         //   if so populate our result array
-//         result.push(firstNumber, secondNumber);
-//       }
-//     }
-//   }
+      //   check if the current sum is equal to the target
+      if (currentSum === target) {
+        //   if so populate our result array
+        result.push(firstNumber, secondNumber);
+      }
+    }
+  }
 
-//   // return result whether empty or populated
-//   return result;
-// };
+  // return result whether empty or populated
+  return result;
+};
 
 // O(n) time complexity, O(n) space complecity
-const twoSum = (array, target) => {
+const twoSumTwo = (array, target) => {
   let result = {};
 
   for (const number of array) {
@@ -54,8 +54,33 @@ const twoSum = (array, target) => {
   return [];
 };
 
+// O(nlogn) time ----- O(1) space
+const twoSumThree = (array, target) => {
+  // sort the array
+  array.sort((a, b) => a - b);
+
+  let left = 0;
+  let right = array.length - 1;
+
+  while (left < right) {
+    const sum = array[left] + array[right];
+
+    if (sum === target) {
+      return [array[left], array[right]];
+    } else if (sum < target) {
+      left++;
+    } else if (sum > target) {
+      right--;
+    }
+  }
+};
+
 // hold the returned values from calling the function twoSum
-const answer = twoSum([3, 5, -4, 8, 11, 1, -1, 6], 10);
+const answerOne = twoSumOne([3, 5, -4, 8, 11, 1, -1, 6], 10);
+const answerTwo = twoSumTwo([3, 5, -4, 8, 11, 1, -1, 6], 10);
+const answerThree = twoSumThree([3, 5, -4, 8, 11, 1, -1, 6], 10);
 
 // sanity check
-console.log(answer);
+console.log(answerOne);
+console.log(answerTwo);
+console.log(answerThree);
