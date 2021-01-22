f119f329a8c8744667fa67974ef0317478a2985a
imxande <alerodriguezlarosa@gmail.com>
2021-01-22 07:13:19 -0500

    implemented a solution

--- array_problems/subSequence.js
+++ array_problems/subSequence.js
@@ -12,3 +12,36 @@ const sequence = [1, 6, -1, 10];
 Sample Output: true
 
 */
+
+/*  
+    Steps 
+    1- Write a function 
+    2- Grab first number of subsequence and compared it with first number of array
+    3- If match then move to the next number in the subsequence
+    4- No matter what we keep moving to the next number in the array
+    5- Return true if we hit the end of the sequence
+*/
+
+// Function
+const validateSeq = (array, sequence) => {
+  // Init pointer to track position
+  let arrIdx = 0;
+  let seqIdx = 0;
+
+  // As long as we are within boundaries of the array and the sequence, perform operations
+  while (array[arrIdx] < array.length && sequence[seqIdx] < sequence.length) {
+    // check if first number of sequence is the same as first number in the original array
+    if (array[arrIdx] === sequence[seqIdx]) {
+      // move the sequence index
+      seqIdx++;
+    }
+    // no matter if we found a match or not we keep moving in the original array
+    arrIdx++;
+  }
+  // once we break out of the loop return true false accordingly
+  return seqIdx === sequence.length;
+};
+
+// Input
+const array = [5, 1, 22, 25, 6, -1, 8, 10];
+const sequence = [1, 6, -1, 10];
