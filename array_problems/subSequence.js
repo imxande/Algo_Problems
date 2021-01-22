5610be88f74c11589102034a762abacf42b2a90e
imxande <alerodriguezlarosa@gmail.com>
2021-01-22 06:56:46 -0500

    added new problem prompt

---
+++ array_problems/subSequence.js
@@ -0,0 +1,14 @@
+/* 
+Given two non-empty array of integers, write a function that determines whether the second array is a subsequence of the first one. A subsequence of an array is a set of numbers that aren't necessarily adjacent but that are
+in the same order as they appear in the array. For instance, the numbers [1, 3, 4] form a subsequence of the 
+array [1, 2, 3, 4], and so do the numbers [2, 4]. Note that a single number in an array and the array itself
+are both a valid subsequences of the array. 
+
+Sample input:
+
+const array = [5, 1, 22, 25, 6, -1, 8, 10];
+const sequence = [1, 6, -1, 10];
+
+Sample Output: true
+
+*/
