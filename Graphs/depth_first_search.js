09ca36471ff90c5f002427b2da7e7849758fa3b8
imxande <alerodriguezlarosa@gmail.com>
2021-01-24 20:34:37 -0500

    another problem

---
+++ Graphs/depth_first_search.js
@@ -0,0 +1,10 @@
+/* 
+    Depth-first Search
+
+You are given a Node class that has a name and an array of Optional children nodes. When put together, node form an 
+acyclic tree-like structure.
+
+Implement the depthFirstSearch method on the Node class, which takes in an empty array, traverses the tree using 
+the DepthFirstSearch approach, stores all the nodes' names in the input array, and returns it.
+
+*/
